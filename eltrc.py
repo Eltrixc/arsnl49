eb183615eacbdc8064244d6bef875022c9000b17
Eltrixc <eltrocco150@gmail.com>
2024-09-09 10:59:37 +0700

    Update eltrc.py

--- eltrc.py
+++ eltrc.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
  
 # Header
-st.header('Rizky :sparkles:')
+st.header('Rizky Setyawan :sparkles:')
 st.subheader('220322601527')
 st.subheader('Plot')
  
