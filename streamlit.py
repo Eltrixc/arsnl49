9a41c0363dbab94f10d05d579cf93f6c762d2306
Eltrixc <eltrocco150@gmail.com>
2024-09-25 09:57:02 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 x = st.number_input("Masukkan Suhu")
 sx = st.text_input("Satuan", "C")
 st.write("Suhu Anda", x, "dengan satuan", sx)
-sx = st.text_input("Konversi", "C")
+sy = st.text_input("Konversi", "C")
 if (sx == "C"):
   pass
 else:
