fa1c85680654a36c889ecf44c4945ff6c4d73a19
Eltrixc <eltrocco150@gmail.com>
2024-09-25 09:56:15 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,4 +3,10 @@ import streamlit as st
 x = st.number_input("Masukkan Suhu")
 sx = st.text_input("Satuan", "C")
 st.write("Suhu Anda", x, "dengan satuan", sx)
-st.write("Kuadratnya", x*x)
+sx = st.text_input("Konversi", "C")
+if (sx == "C"):
+  pass
+else:
+  pass
+
+st.write("Hasil Konversi dari ", x, sx, "adalah ...", sy)
